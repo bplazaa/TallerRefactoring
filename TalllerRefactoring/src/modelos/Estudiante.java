770af3099943812f5923a0e6e7f9e5c60763df4c
Bryan Plaza <bryanplaza_@hotmail.com>
2020-08-13 19:20:14 -0500

    Long Class in Estudiante fixed

--- TalllerRefactoring/src/modelos/Estudiante.java
+++ TalllerRefactoring/src/modelos/Estudiante.java
@@ -78,40 +78,7 @@ public class Estudiante{
         this.telefono = telefono;
     }
     
-    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    public double CalcularNotaInicial(double nexamen,double ndeberes, double nlecciones, double ntalleres){
-        		double notaInicial=0;
-                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
-                double notaPractico=(ntalleres)*0.20;
-                notaInicial=notaTeorico+notaPractico;
- 
-        return notaInicial;
-    }
     
-    //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    
-    public double CalcularNotaFinal(double nexamen,double ndeberes, double nlecciones, double ntalleres){
-        		double notaFinal=0;
-                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
-                double notaPractico=(ntalleres)*0.20;
-                notaFinal=notaTeorico+notaPractico;
-            
-        
-        return notaFinal;
-    }
-    
-    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
-    public double CalcularNotaTotal(Paralelo p){
-        double notaTotal=0;
-        for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                notaTotal=(p.getMateria().notaInicial+p.getMateria().notaFinal)/2;
-                
-            }
-        }
-        return notaTotal;
-        
-    }
 }
         
     
